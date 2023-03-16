a884df7a8f959ef2cb5d9e4e587507ef2ae33c4e
Edson Tofolo <edsontofolo@gmail.com>
2023-03-16 11:11:35 -0300

    testify with mock

--- testing/testify/tax_test.go
+++ testing/testify/tax_test.go
@@ -1,6 +1,7 @@
 package testify
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -15,4 +16,20 @@ func TestCalculateTax(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.Equal(t, 0.0, tax)
+}
+
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil).Once()
+	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
+
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.NoError(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err)
+
+	repository.AssertExpectations(t)
+	repository.AssertCalled(t, "SaveTax", 10.0)
+	repository.AssertNumberOfCalls(t, "SaveTax", 1)
 }
